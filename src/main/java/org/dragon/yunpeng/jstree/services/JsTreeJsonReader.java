40db2e39055ea5c72c38bdcf01f2d18d349f07b2
Yunpeng Li <yli4@umbc.edu>
2025-08-14 18:11:44 -0400

    Added local js and css

--- src/main/java/org/dragon/yunpeng/jstree/services/JsTreeJsonReader.java
+++ src/main/java/org/dragon/yunpeng/jstree/services/JsTreeJsonReader.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.dragon.yunpeng.jstree.dtos.JsTreeNode;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.IOException;
 
 public class JsTreeJsonReader {
-	public static void main(String[] args) throws Exception {
+
+	public void constructJsTreeNodeListFromJsonFile() throws IOException, StreamReadException, DatabindException {
 		ObjectMapper mapper = new ObjectMapper();
 
 		// Read JSON from a file (replace with your file path)
 		String path = "C:\\Projects\\SpringBootJsTree\\mini-symboltree-data.json";
-		
+
 		List<JsTreeNode> nodes = mapper.readValue(new File(path), new TypeReference<List<JsTreeNode>>() {
 		});
 
@@ -24,7 +28,7 @@ public class JsTreeJsonReader {
 		}
 	}
 
-	private static String repeat(String s, int count) {
+	private String repeat(String s, int count) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < count; i++) {
 			sb.append(s);
@@ -32,7 +36,7 @@ public class JsTreeJsonReader {
 		return sb.toString();
 	}
 
-	private static void printNode(JsTreeNode node, int level) {
+	private void printNode(JsTreeNode node, int level) {
 		String indent = repeat("  ", level);
 		System.out.println(indent + node.getText() + " - " + node.getData());
 
